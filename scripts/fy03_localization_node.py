46d1074f200d549b38e0e82198dd0271b964df8c
Joey Yang <yangj30@mcmaster.ca>
2020-10-30 13:31:20 -0400

    link nav msgs, and setup subscriber and publisher methods

--- scripts/fy03_localization_node.py
+++ scripts/fy03_localization_node.py
@@ -1,31 +1,51 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import Int32
+# from ros_dwm1001.msg import Tag, Anchor
+from sensor_msgs.msg import Imu
+from nav_msgs/Odometry import Odometry
 
-class Dwm1001Node(object):
+class LocalizationNode(object):
     def __init__(self):
-        rospy.init_node('echoer')
+        #
+        # Instantiate Localization object.
+        # self.localization = Localization()
+        #
 
-        self.pub = rospy.Publisher('/out_value', Int32, latch=True)
-        rospy.Subscriber('/in_value', Int32, self.update_value)
-
-    def update_value(self, msg):
-        self.value = msg.data
+        # self.tag_msg_ = Tag()
+        # self.anchor_msg_ = Anchor()
+        self.imu_msg_ = Imu()
+        self.fused_pose_msg_ = Odometry()
+        
+        # self.uwb_tag_sub_ = rospy.Subscriber("/uwb_tag", Tag, self.tagCallback)
+        # self.uwb_anchor_sub_ = rospy.Subscriber("/uwb_anchor", Anchor, self.anchorCallback)
+        self.imu_sub_ = rospy.Subscriber("/imu", Imu, self.imuCallback)
+        self.fused_pose_pub_ = rospy.Publisher('/fused_pose', Odometry, queue_size = 1)
 
     def run(self):
-        r = rospy.Rate(10)
-        while not rospy.is_shutdown():
-            self.pub.publish(self.value)
-            r.sleep()
+        rospy.spin()
+    
+    '''
+    def tagCallback(self, tag_msg):
+        self.publishFusedPose(fused_pose)
+
+    def anchorCallback(self, anchor_msg):
+        self.publishFusedPose(fused_pose)
+        '''
+
+    def imuCallback(self, imu_msg):
+        # self.publishFusedPose(fused_pose)
+
+    def publishFusedPose(self, fused_pose):
+        # self.fused_pose_pub_.publish(self.fused_pose_msg_)
+        pass
 
 if __name__ == '__main__':
     rospy.init_node("fy03_localization_node")
-    rate = rospy.Rate(15)
+    localization_node = LocalizationNode()
     
     try:
-        while not rospy.is_shutdown():
-            rate.sleep()
+        localization_node.run()
     except rospy.ROSInterruptException:
         pass
 
