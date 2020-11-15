ae0a15d513b4738c8d1fbea5cbddca0f7201cf60
Joey Yang <joeyyang.ai@gmail.com>
2020-11-15 20:43:46 +0000

    some fixes

--- scripts/imu_position_estimation_node.py
+++ scripts/imu_position_estimation_node.py
@@ -10,17 +10,16 @@
 import rospy
 from sensor_msgs.msg import Imu
 
-position = 
-{
+position = {
   "x": 0,
   "y": 0,
   "z": 0
 }
-position = [0, 0, 0]
 previous_time = 0
 total_time = 0
 accuracy_5cm = True
 accuracy_10cm = True
+start = False
 
 def accuracyTest5Cm():
   global position, total_time, accuracy_5cm
@@ -48,66 +47,70 @@ def getRotationMatrix(quaternion):
   r_32 = 2*s*(y*z + x*w)
   r_33 = 1 - 2*s*(x**2 + y**2)
   
-  rotation_matrix = 
-  [[r_11, r_12, r_13],
-  [r_21, r_22, r_23],
-  [r_31, r_32, r_33]]
+  rotation_matrix = [[r_11, r_12, r_13],
+  		    [r_21, r_22, r_23],
+  		    [r_31, r_32, r_33]]
   
   return rotation_matrix
 
 def imuCallback(imu_msg):
-  global position, previous_time, total_time, accuracy_5cm, accuracy_10cm
+  global position, previous_time, total_time, accuracy_5cm, accuracy_10cm, start
   
-  message_time = imu_msg.header.stamp.to_sec()
-  rospy.loginfo("Message sequence: %f, Message time: %f", imu_msg.header.seq, message_time)
-  time_difference = message_time - previous_time
-  total_time += time_difference 
-  # rospy.loginfo("Time difference: %f", time_difference)
-  # Set previous_time to (current) message_time for next callback.
-  previous_time = message_time
-
-  linear_acceleration_x = imu_msg.linear_acceleration.x
-  linear_acceleration_y = imu_msg.linear_acceleration.y
-  linear_acceleration_z = imu_msg.linear_acceleration.z
-  rospy.loginfo("Linear Acceleration X: %f, Y: %f, Z: %f", linear_acceleration_x, linear_acceleration_y, linear_acceleration_z)
-
-  linear_velocity_x = linear_acceleration_x * time_difference
-  linear_velocity_y = linear_acceleration_y * time_difference
-  linear_velocity_z = linear_acceleration_z * time_difference
-  rospy.loginfo("Linear Velocity X: %f, Y: %f, Z: %f", linear_velocity_x, linear_velocity_y, linear_velocity_z)
-
-  linear_displacement_x = linear_velocity_x * time_difference + 0.5 * linear_acceleration_x * time_difference * time_difference
-  linear_displacement_y = linear_velocity_y * time_difference + 0.5 * linear_acceleration_y * time_difference * time_difference
-  linear_displacement_z = linear_velocity_z * time_difference + 0.5 * linear_acceleration_z * time_difference * time_difference
-  rospy.loginfo("Linear Displacement X: %f, Y: %f, Z: %f", linear_displacement_x, linear_displacement_y, linear_displacement_z)
-
-  #linear_displacement_x = linear_velocity_x * time_difference
-  #linear_displacement_y = linear_velocity_y * time_difference
-  #linear_displacement_z = linear_velocity_z * time_difference
-
-  position["x"] += linear_displacement_x
-  position["y"] += linear_displacement_y
-  position["z"] += linear_displacement_z
-
-  # Determine time taken to fail 5cm accuracy due to IMU drift.
-  accuracyTest5Cm()
-  # Determine time taken to fail 10cm accuracy due to IMU drift.
-  accuracyTest10Cm()
-
-  rospy.loginfo("Position X: %f, Y: %f, Z: %f", position["x"], position["y"], position["z"])
   rospy.loginfo("--------------------------------------------------------------------------")
+
+  if not start:
+   # Set starting time stamp to current time.
+    previous_time = rospy.get_time()
+    message_time = imu_msg.header.stamp.to_sec()
+    rospy.loginfo("Initial time: %f:", previous_time)
+    rospy.loginfo("Initial time: %f:", message_time)
+    start = True
+  else:
+    message_time = imu_msg.header.stamp.to_sec()
+    rospy.loginfo("Message sequence: %f, Message time: %f", imu_msg.header.seq, message_time)
+    time_difference = message_time - previous_time
+    total_time += time_difference 
+    rospy.loginfo("Time difference: %f", time_difference)
+    # Set previous_time to (current) message_time for next callback.
+    previous_time = message_time
+
+    linear_acceleration_x = imu_msg.linear_acceleration.x
+    linear_acceleration_y = imu_msg.linear_acceleration.y
+    linear_acceleration_z = imu_msg.linear_acceleration.z
+    rospy.loginfo("Linear Acceleration X: %f, Y: %f, Z: %f", linear_acceleration_x, linear_acceleration_y, linear_acceleration_z)
+
+    linear_velocity_x = linear_acceleration_x * time_difference
+    linear_velocity_y = linear_acceleration_y * time_difference
+    linear_velocity_z = linear_acceleration_z * time_difference
+    rospy.loginfo("Linear Velocity X: %f, Y: %f, Z: %f", linear_velocity_x, linear_velocity_y, linear_velocity_z)
+
+    #linear_displacement_x = linear_velocity_x * time_difference + 0.5 * linear_acceleration_x * time_difference * time_difference
+    #linear_displacement_y = linear_velocity_y * time_difference + 0.5 * linear_acceleration_y * time_difference * time_difference
+    #linear_displacement_z = linear_velocity_z * time_difference + 0.5 * linear_acceleration_z * time_difference * time_difference
+    linear_displacement_x = linear_velocity_x * time_difference
+    linear_displacement_y = linear_velocity_y * time_difference
+    linear_displacement_z = linear_velocity_z * time_difference
+    rospy.loginfo("Linear Displacement X: %f, Y: %f, Z: %f", linear_displacement_x, linear_displacement_y, linear_displacement_z)
+
+    position["x"] += linear_displacement_x
+    position["y"] += linear_displacement_y
+    position["z"] += linear_displacement_z
+
+    rospy.loginfo("Position X: %f, Y: %f, Z: %f", position["x"], position["y"], position["z"])
+    
+    # Determine time taken to fail 5cm accuracy due to IMU drift.
+    accuracyTest5Cm()
+    # Determine time taken to fail 10cm accuracy due to IMU drift.
+    accuracyTest10Cm()
 
 if __name__ == '__main__':
  
-  rospy.init_node("imu_position_estimation_node") 
-  rospy.Subscriber("/imu", Imu, imuCallback)
+    rospy.init_node("imu_position_estimation_node") 
+    rospy.Subscriber("/imu", Imu, imuCallback)
 
-  rospy.loginfo("Initial position X: %f, Y: %f, Z: %f", position["x"], position["y"], position["z"])
-  # Set starting time stamp to current time.
-  previous_time = rospy.get_time()
-  rospy.loginfo("Initial time: %f:", previous_time)
+    rospy.loginfo("Initial position X: %f, Y: %f, Z: %f", position["x"], position["y"], position["z"])
  
-  rospy.spin()
+    rospy.spin()
 
 
 
