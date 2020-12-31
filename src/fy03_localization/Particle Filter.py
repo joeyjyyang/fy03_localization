aba9e8d4db5e505bb2ee653779f9930e0b36af9a
parkec11 <70435944+parkec11@users.noreply.github.com>
2020-12-31 10:50:57 -0500

    Add files via upload

--- src/fy03_localization/Particle Filter.py
+++ src/fy03_localization/Particle Filter.py
@@ -8,20 +8,37 @@ from numpy import *
 # Prediction step.
 # Get motion data (control inputs) from IMU,
 # and predict the new positions of all particles.
-def predict(num_particles: int, x_vals: list, y_vals: list, u: list, dt = 1.) -> list:
+def predict(x_vel: float, y_vel: float, x_vals: list, y_vals: list, u: list, dt = 1.) -> list:
     linear_acceleration_x = u[0]
     linear_acceleration_y = u[1]
 
-    linear_velocity_x = linear_acceleration_x * dt
-    linear_velocity_y = linear_acceleration_y * dt
+    linear_velocity_x = x_vel + (linear_acceleration_x * dt)
+    linear_velocity_y = y_vel + (linear_acceleration_y * dt)
     
     linear_displacement_x = linear_velocity_x * dt
     linear_displacement_y = linear_velocity_y * dt
 
-    for i in range(num_particles):
+    for i in range(0, len(x_vals)):
         x_vals[i] += linear_displacement_x
-	y_vals[i] += linear_displacement_y
+        y_vals[i] += linear_displacement_y
 
+    return linear_displacement_x, linear_displacement_y, linear_velocity_x, linear_velocity_y
+
+def string_to_list(input_str: str):
+    iterator = 0
+    ret_list = []
+    temp_val = ""
+    while(input_str[iterator] != "\n"):
+        if(input_str[iterator] == ","):
+            ret_list.append(float(temp_val))
+            temp_val = ""
+        else:
+            temp_val = temp_val + input_str[iterator]
+        iterator += 1
+    ret_list.append(float(temp_val))
+    return ret_list
+
+    
 def random_particles(num_particles: int, x_range: tuple, y_range:tuple) -> list:
     ##make particles
     x_out = []
@@ -53,13 +70,13 @@ def normalize_weights(weights: list) -> list:
         normalized_weights.append(weights[j]/total_weight)
     return normalized_weights
 
-def p2p_distance(point1: tuple, point2: tuple):
+def p2p_distance(point1: list, point2: tuple):
     x_diff = float(point1[0]-point2[0])
     y_diff = float(point1[1]-point2[1])
     distance = sqrt((x_diff**2) + (y_diff**2))
     return distance
 
-def assign_weights(measurement: tuple, x_vals: list, y_vals: list, cov: float) -> list:
+def assign_weights(measurement: list, x_vals: list, y_vals: list, cov: float) -> list:
     location = 0
     weights = []
     distance = 0
@@ -93,21 +110,44 @@ def re_sample(x_values: list, y_values: list, normalized_weights: list, num_part
     return x_return, y_return, weights_return
         
 def main():
+    current_pos = [5,5]
     num_particles = 10000
+    accel_vector = []
+    x_displace = 0
+    y_displace = 0
+    x_vel = 0
+    y_vel = 0
     x_range = (0,10)
     y_range = (0,10)
     x_vals = []
     y_vals = []
+    accel_tuple = ()
     weights = []
     current_estimate = ()
     normalized_weights = []
     UWB_covariance = 0.3
+    myfile = open("traj1.txt", 'r')
 
     x_vals, y_vals = random_particles(num_particles, x_range, y_range)
     plot_particles(x_vals, y_vals)
 
-    for i in range(0,5):
+    ##readfile
+    accel_tuple = myfile.readline()
+    print(accel_tuple)
 
+
+    ##while file not at end
+
+    for i in myfile:
+        print(i)
+        accel_vector = string_to_list(i)
+        print(accel_vector)
+
+        x_displace, y_displace, x_vel, y_vel = predict(x_vel, y_vel, x_vals, y_vals, accel_vector)
+
+
+        current_pos[0] = current_pos[0] + x_displace
+        current_pos[1] = current_pos[1] + y_displace
         ##make
 
         #print(x_vals, y_vals)
@@ -115,7 +155,7 @@ def main():
         ##plot
  
 
-        weights = assign_weights((5,5), x_vals, y_vals, UWB_covariance)
+        weights = assign_weights(current_pos, x_vals, y_vals, UWB_covariance)
         normalized_weights = normalize_weights(weights)
 
         current_estimate = find_current_estimate(x_vals, y_vals, normalized_weights)
