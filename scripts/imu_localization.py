31d7a18924241ac8fcd1eaae227f6230b58b46cb
Joey Yang <joeyyang.ai@gmail.com>
2021-03-02 06:19:23 +0000

    imu localization node

--- scripts/imu_localization.py
+++ scripts/imu_localization.py
@@ -9,6 +9,7 @@
 
 import rospy
 from sensor_msgs.msg import Imu
+from nav_msgs.msg import Odometry
 
 position = {
   "x": 0,
@@ -21,6 +22,11 @@ accuracy_5cm = True
 accuracy_10cm = True
 start = False
 
+odom_msg = Odometry()
+odom_pub = rospy.Publisher("/odom", Odometry, queue_size=1)
+
+
+"""
 def accuracyTest5Cm():
   global position, total_time, accuracy_5cm
   if (accuracy_5cm and (abs(position["x"]) >= 0.05 or abs(position["y"]) >= 0.05 or abs(position["z"]) >= 0.05)):
@@ -53,23 +59,22 @@ def getRotationMatrix(quaternion):
   
   return rotation_matrix
 
-def imuCallback(imu_msg):
-  global position, previous_time, total_time, accuracy_5cm, accuracy_10cm, start
-  
-  rospy.loginfo("--------------------------------------------------------------------------")
+"""
 
+def imuCallback(imu_msg):
+  global position, previous_time, total_time, accuracy_5cm, accuracy_10cm, start, odom_pub, odom_msg
+  
   if not start:
    # Set starting time stamp to current time.
-    previous_time = rospy.get_time()
-    message_time = imu_msg.header.stamp.to_sec()
+    previous_time = imu_msg.header.stamp.to_sec()
     rospy.loginfo("Initial time: %f:", previous_time)
-    rospy.loginfo("Initial time: %f:", message_time)
     start = True
+  
   else:
     message_time = imu_msg.header.stamp.to_sec()
     #rospy.loginfo("Message sequence: %f, Message time: %f", imu_msg.header.seq, message_time)
     time_difference = message_time - previous_time
-    total_time += time_difference 
+    #total_time += time_difference 
     #rospy.loginfo("Time difference: %f", time_difference)
     # Set previous_time to (current) message_time for next callback.
     previous_time = message_time
@@ -77,12 +82,10 @@ def imuCallback(imu_msg):
     linear_acceleration_x = imu_msg.linear_acceleration.x
     linear_acceleration_y = imu_msg.linear_acceleration.y
     linear_acceleration_z = imu_msg.linear_acceleration.z
-    rospy.loginfo("Linear Acceleration X: %f, Y: %f, Z: %f", linear_acceleration_x, linear_acceleration_y, linear_acceleration_z)
 
     linear_velocity_x = linear_acceleration_x * time_difference
     linear_velocity_y = linear_acceleration_y * time_difference
     linear_velocity_z = linear_acceleration_z * time_difference
-    rospy.loginfo("Linear Velocity X: %f, Y: %f, Z: %f", linear_velocity_x, linear_velocity_y, linear_velocity_z)
 
     #linear_displacement_x = linear_velocity_x * time_difference + 0.5 * linear_acceleration_x * time_difference * time_difference
     #linear_displacement_y = linear_velocity_y * time_difference + 0.5 * linear_acceleration_y * time_difference * time_difference
@@ -90,28 +93,25 @@ def imuCallback(imu_msg):
     linear_displacement_x = linear_velocity_x * time_difference
     linear_displacement_y = linear_velocity_y * time_difference
     linear_displacement_z = linear_velocity_z * time_difference
-    rospy.loginfo("Linear Displacement X: %f, Y: %f, Z: %f", linear_displacement_x, linear_displacement_y, linear_displacement_z)
 
     position["x"] += linear_displacement_x
     position["y"] += linear_displacement_y
     position["z"] += linear_displacement_z
 
-    rospy.loginfo("Position X: %f, Y: %f, Z: %f", position["x"], position["y"], position["z"])
-    
-    # Determine time taken to fail 5cm accuracy due to IMU drift.
-    #accuracyTest5Cm()
-    # Determine time taken to fail 10cm accuracy due to IMU drift.
-    #accuracyTest10Cm()
+    odom_msg.header.stamp = rospy.get_rostime()
+    odom_msg.header.frame_id = "imu_link"
+
+    odom_msg.pose.pose.position.x = position["x"]
+    odom_msg.pose.pose.position.y = position["y"]
+    odom_msg.pose.pose.position.z = position["z"]
+ 
+    odom_pub.publish(odom_msg)
 
 if __name__ == '__main__':
  
-    rospy.init_node("imu_position_estimation_node") 
-    imu_sub = rospy.Subscriber("/imu", Imu, imuCallback)
-
+    rospy.init_node("imu_localization_node") 
     rospy.loginfo("Initial position X: %f, Y: %f, Z: %f", position["x"], position["y"], position["z"])
  
+    imu_sub = rospy.Subscriber("/imu/data", Imu, imuCallback)
+
     rospy.spin()
-
-
-
-
